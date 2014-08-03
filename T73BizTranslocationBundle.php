9f490ae0b064056c85dd9ca4d0008f48c6be6754
rchaplin <ron.g.chaplin@gmail.com>
2014-08-02 22:06:56 -0700

    Bumping version Changing email address

--- T73BizTranslocationBundle.php
+++ T73BizTranslocationBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class T73BizTranslocationBundle
- * @author Ronald Chaplin <ron.g.chaplin@gmail.com>
+ * @author Ronald Chaplin <rchaplin@t73.biz>
  */
 class T73BizTranslocationBundle extends Bundle
 {
