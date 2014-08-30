efeda66d224df48562078d578889631ff94f0909
rchaplin <ron.g.chaplin@gmail.com>
2014-08-29 21:33:10 -0700

    stuff

--- Tests/Controller/TranslateControllerTest.php
+++ Tests/Controller/TranslateControllerTest.php
@@ -9,8 +9,16 @@ namespace T73Biz\Bundle\TranslocationBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class TranslateControllerTest
+ *
+ * @author Ronald Chaplin <rchaplin@t73.biz>
+ */
 class TranslateControllerTest extends WebTestCase
 {
 
+    public function testIndexAction()
+    {
+        return true;
+    }
 }
- 
